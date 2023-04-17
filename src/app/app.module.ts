b13a5ccfe3b1a581a81fd85bea6dac80fda884cd
samat <somych1@gmail.com>
2023-04-17 15:55:54 -0500

    avatar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -15,7 +14,6 @@ import { MatDividerModule } from '@angular/material/divider'
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ProjectsComponent
   ],
   imports: [
